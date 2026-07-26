fa6ec9daad14f66f2c4f671d6e7c19d550433eee
sarsennurasyll <160136040+sarsennurasyll@users.noreply.github.com>
2026-07-26 19:04:49 +0500

    chore: инициализация основы Spring Boot проекта

---
+++ src/main/java/com/esep/EsepApplication.java
@@ -0,0 +1,12 @@
+package com.esep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsepApplication.class, args);
+    }
+}
